8ed76020d11958d11df4cf7c4e7934d016a10d05
Roopesh Naik <rnaik77@stanford.edu>
2021-07-16 18:06:55 +0530

    phpcs errors fix

--- web/modules/custom/connecting_links/src/Form/ConnectingLinksForm.php
+++ web/modules/custom/connecting_links/src/Form/ConnectingLinksForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class ConnectingLinksForm.
+ * Class ConnectingLinks Form.
  */
 class ConnectingLinksForm extends EntityForm {
 
